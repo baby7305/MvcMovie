2a737677aa12a159271808334f216bb615df2d95
baby7305 <songhaibei7305@163.com>
2019-04-05 21:37:17 +0800

    自动生成数据

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -18,6 +18,7 @@ namespace MvcMovie.Models {
                         Title = "When Harry Met Sally",
                             ReleaseDate = DateTime.Parse ("1989-2-12"),
                             Genre = "Romantic Comedy",
+                            Rating = "R",
                             Price = 7.99M
                     },
 
@@ -25,6 +26,7 @@ namespace MvcMovie.Models {
                         Title = "Ghostbusters ",
                             ReleaseDate = DateTime.Parse ("1984-3-13"),
                             Genre = "Comedy",
+                            Rating = "R",
                             Price = 8.99M
                     },
 
@@ -32,6 +34,7 @@ namespace MvcMovie.Models {
                         Title = "Ghostbusters 2",
                             ReleaseDate = DateTime.Parse ("1986-2-23"),
                             Genre = "Comedy",
+                            Rating = "R",
                             Price = 9.99M
                     },
 
@@ -39,6 +42,7 @@ namespace MvcMovie.Models {
                         Title = "Rio Bravo",
                             ReleaseDate = DateTime.Parse ("1959-4-15"),
                             Genre = "Western",
+                            Rating = "R",
                             Price = 3.99M
                     }
                 );
