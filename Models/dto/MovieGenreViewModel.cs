5d818d9fe38d664ad1c2f820c67544b1d748c8b4
baby7305 <songhaibei7305@163.com>
2019-04-05 20:29:37 +0800

    加一个dto

---
+++ Models/dto/MovieGenreViewModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MvcMovie.Models {
+    public class MovieGenreViewModel {
+        public List<Movie> Movies;
+        public SelectList Genres;
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
