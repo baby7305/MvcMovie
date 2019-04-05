58c0041c5105d9d6d81da884212b42fe5df19eb1
baby7305 <songhaibei7305@163.com>
2019-04-05 15:01:52 +0800

    增加ID语法糖
    http://localhost:5000/HelloWorld/welcome/3?name=baby

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -14,8 +14,8 @@ namespace MvcMovie.Controllers {
         // GET: /HelloWorld/Welcome/ 
         // Requires using System.Text.Encodings.Web;
 
-        public string Welcome (string name, int numTimes = 1) {
-            return HtmlEncoder.Default.Encode ($"Hello {name}, NumTimes is: {numTimes}");
+        public string Welcome (string name, int ID = 1) {
+            return HtmlEncoder.Default.Encode ($"Hello {name}, ID: {ID}");
         }
     }
 }
